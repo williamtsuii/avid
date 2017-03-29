1fd11200c7cdac17f65904c0f45c19feaaff081c
williamtsuii <william_xu@live.com>
2017-03-29 09:22:53 -0700

    Sequencer class

---
+++ src/Anchor.java
@@ -0,0 +1,9 @@
+/**
+ * Created by william on 2017-03-29.
+ */
+public class Anchor {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Parser.java
@@ -0,0 +1,11 @@
+/**
+ * Created by william on 2017-03-29.
+ */
+public class Parser {
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
