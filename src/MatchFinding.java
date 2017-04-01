c01fbf452931877113d4ca42f4560ee847406a39
williamtsuii <william_xu@live.com>
2017-04-01 16:40:42 -0700

    ...

--- src/MatchFinding.java
+++ src/MatchFinding.java
@@ -5,30 +5,25 @@ import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
  */
 public class MatchFinding extends Parser {
 
-    public MatchFinding() {
-        super();
-    }
-
     Parser parsedFile = new Parser();
     String[] description = parsedFile.getDescription();
     String[] sequence = parsedFile.getSequence();
+    public MatchFinding() {
+        super();
+    }
 
     //TODO: implement data structure for suffix tree
 
     //TODO: concatenating helper func.
         // place character 'N' between the two sequences
 
-    public static void main(String args[]) {
-
-    }
-
     //TODO: maximal repeated substring function
         // used once for each sequence
 
     //TODO: maximal matches function
 
+    public static void main(String args[]) {
 
-
-
+    }
 
 }
